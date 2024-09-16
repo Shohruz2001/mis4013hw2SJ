b1368105bb0b84609a2c814a5092b5f09fbf7869
Shohruz2001 <123128209+Shohruz2001@users.noreply.github.com>
2024-09-16 11:32:28 -0500

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,4 +1,4 @@
-    </div>
+    </div> <!-- Close the container div -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwvvtgBNo3bZJLYd80VXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmn1MuBnhbgnk" crossorigin="anonymous"></script>
 </body>
 </html>
